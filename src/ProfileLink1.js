ccd810072d7fcfd6d383c75dc630408e29047abf
Anton Kononchenko <akononche@gmail.com>
2024-11-24 12:08:09 +0300

    urls

--- src/ProfileLink1.js
+++ src/ProfileLink1.js
@@ -8,7 +8,7 @@ const ProfileButton1 = () => {
         setLoading(true);
         try {
             // Отправляем запрос на сервер для получения ссылки
-            const response = await fetch('https://marcuspro.life/api/get-profile-link1');
+            const response = await fetch('https://marcuspro.life/api/get-profile-link-alex');
             const data = await response.json();
             // Перенаправляем пользователя на полученную ссылку
             window.location.href = data.link;
